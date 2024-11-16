9280e4e916f4e52adffc203d012853e40af5a5b9
Jeonha Kang <kangjeonha@gmail.com>
2024-11-16 17:11:03 +0900

    second-commit

--- src/page/Loginpage.js
+++ src/page/Loginpage.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import './Loginpage.css'; // CSS 파일 분리
 
 const Loginpage = () => {
   return (
-    <div>Loginpage</div>
-  )
-}
+    <div className="login-container">
+      <h1 className="login-title">Welcome Back!</h1>
+      <form className="login-form">
+        <input type="text" placeholder="Username" className="login-input" />
+        <input type="password" placeholder="Password" className="login-input" />
+        <button type="submit" className="login-button">Login</button>
+      </form>
+    </div>
+  );
+};
 
-export default Loginpage
+export default Loginpage;
