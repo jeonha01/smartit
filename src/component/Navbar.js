c1bc6bc6510d329fd98cf78eb3396240989b2013
Jeonha Kang <kangjeonha@gmail.com>
2024-11-17 20:51:02 +0900

    tenth-commit

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../App.css';
 
 const Navbar = () => {
     const menuList = ['Home', '스케줄러', '공부방', '랭킹', '마이페이지'];
+    const [activeMenu, setActiveMenu] = useState('Home'); // 초기값: 'Home'
+
     return (
         <nav className='navbar'>
             <ul className='menu-list'>
                 {menuList.map((menu, index) => (
-                    <li key={index}>{menu}</li>
+                    <li
+                        key={index}
+                        className={`menu-item ${activeMenu === menu ? 'active' : ''}`} // 활성화된 메뉴에 클래스 추가
+                        onClick={() => setActiveMenu(menu)} // 클릭 시 상태 업데이트
+                    >
+                        {menu}
+                    </li>
                 ))}
             </ul>
         </nav>
@@ -15,3 +23,5 @@ const Navbar = () => {
 };
 
 export default Navbar;
+
+
